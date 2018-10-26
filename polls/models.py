96515e1446f20ed805249d37cabf5c128a65f398
Chihiro <632207812@qq.com>
2018-10-26 16:26:33 +0800

    6666

--- polls/models.py
+++ polls/models.py
@@ -1,20 +1,23 @@
 from django.db import models
-
-# Create your models here.
-from django.db import models
-from django.utils import timezone
 import datetime
+from django.utils import timezone
+# Create your models here.
 
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
 
-    def __str__(self):  # 在python2版本中使用的是__unique__
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
+
+    def __str__(self):
         return self.question_text
 
-    def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
 
 class Choice(models.Model):
--- polls/urls.py
+++ polls/urls.py
@@ -1,23 +1,12 @@
 from django.conf.urls import url
-from django.contrib import admin
 from . import views
 
-# urlpatterns = [
-#     url(r'^$', views.index, name='index'),
-# ]
-# urlpatterns = [
-#     url(r'^my/set/', admin.site.urls),
-#     url(r'^$', views.index, name='index'),
-# ]
 app_name = 'polls'
+
+
 urlpatterns = [
-    # ex: /polls/
-    url(r'^$', views.index, name='index'),
-    # url(r'^my/set/', admin.site.urls),
-    # ex: /polls/5/
-    url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
-    # ex: /polls/5/results/
-    url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
-    # ex: /polls/5/vote/
+    url(r'^$', views.IndexView.as_view(), name='index'),
+    url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
+    url(r'^(?P<pk>[0-9]+)/results/$', views.ResultsView.as_view(), name='results'),
     url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
 ]
